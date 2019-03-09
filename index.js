6146baf25d318e681a19aff502146dced8379d18
Jose Fernandez-Castano <josefernandezcastano@gmail.com>
2019-03-09 16:46:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // Code your solution in this file!
 const companyName = 'Scuber'
 companyName = 'specialCompany'
-let mostProfitableNeighborhood = 'Chelsea'
 mostProfitableNeighborhood = 'Upper West Side'
-let companyCeo = 'Susan Smith'
 companyCeo = 'Lauren Hart' 
