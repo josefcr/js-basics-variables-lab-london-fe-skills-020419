70ef4e69705780c9b9a3b6e3c17d5e0bd0e74254
Jose Fernandez-Castano <josefernandezcastano@gmail.com>
2019-03-09 16:46:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ companyName = 'specialCompany'
 let mostProfitableNeighborhood = 'Chelsea'
 mostProfitableNeighborhood = 'Upper West Side'
 let companyCeo = 'Susan Smith'
-companyCeo = 
+companyCeo = 'Lauren Hart' 
